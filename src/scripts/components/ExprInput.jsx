c88f1afa82f4d42726859cfb86eb4677f1442edf
s_anno <amo.20325@gmail.com>
2017-05-28 20:23:47 +0800

    add Tokenizer

--- src/scripts/components/ExprInput.jsx
+++ src/scripts/components/ExprInput.jsx
@@ -1,12 +1,27 @@
 "use strict";
 
 import React from "react"
+import Tokenizer from "models/Tokenizer"
 
 export default class ExprInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.tokenizer = new Tokenizer();
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    console.log(">>>>>>>>>>>>>>>>>>>");
+    console.log(e.target.value);
+    const token = this.tokenizer.tokenize(e.target.value);
+    console.log(token);
+    console.log("<<<<<<<<<<<<<<<<<<<");
+  }
+
   render() {
     return (
       <div className="input">
-        <input type="text" />
+        <input type="text" onChange={this.handleChange} />
       </div>
     );
   }
---
+++ src/scripts/models/Tokenizer.js
@@ -0,0 +1,59 @@
+"use strict";
+
+export default class Tokenizer {
+  tokenize(code) {
+    var chunk = code;
+    var i = 0;
+    var tokens = []
+    while(chunk) {
+      const consumed = this.numberToken(chunk, tokens) ||
+        this.reservedCharToken(chunk, tokens) ||
+        this.theOtherToken(chunk, tokens);
+      i += consumed;
+      chunk = code.slice(i);
+    }
+
+    return tokens;
+  }
+
+  numberToken(chunk, tokens) {
+    const match = chunk.match(/^\d+/);
+    if (!match) return 0;
+    tokens.push({
+      name: "number",
+      value: Number(match[0])
+    });
+    return match[0].length;
+  }
+
+  reservedCharToken(chunk, tokens) {
+    const name = RESERVED_CHAR_NAMES[chunk[0]];
+    if (!name) return 0;
+    tokens.push({
+      name: name,
+      value: chunk[0]
+    })
+    return 1;
+  }
+
+  theOtherToken(chunk, tokens) {
+    const match = chunk.match(/^[^-+*\/()0-9]+/);
+    if (!match) return 0;
+    return match[0].length;
+  }
+
+  f(code, i) {
+  }
+}
+
+const RESERVED_CHAR_NAMES = {
+  "(": "PARENTHESES_OPEN",
+  ")": "PARENTHESES_CLOSE",
+  "+": "ADD",
+  "-": "SUB",
+  "*": "MUL",
+  "/": "DIV"
+}
+
+const THEOTHER = /^[^-+*\/()0-9]+/;
+
